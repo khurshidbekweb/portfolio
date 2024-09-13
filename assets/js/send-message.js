d33208d138223609f387b05430d12c39d27a8130
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-09-13 18:46:02 +0500

    finished contact send text

--- assets/js/send-message.js
+++ assets/js/send-message.js
@@ -1,5 +1,45 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log(11);
     
+    const form = document.getElementById('form-contact'),
+        btnMessage = document.getElementById('button-message'),
+        telegramTokenBot = "7243637585:AAHpFEkQM4WzMtT5GsIq3gjWCK7aDnWRdEY",
+        chatId = "6551895968"
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const formData = new FormData(form)
+        const object = {}
+        const message = {
+            loading: "Loading...",
+            success: "Succcess",
+            fialer: "Erorr🤷‍♂️"
+        }
+        formData.forEach((value, key) => {
+            object[key] = value
+        })
+
+        btnMessage.textContent = message.loading
+        
+        fetch(`https://api.telegram.org/bot${telegramTokenBot}/sendMessage`, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: ` Name: ${object.Name}
+                        Email: ${object.Email} 
+                        Subject: ${object.Subject}
+                        Message: ${object.Message}
+                `
+                })
+            }
+        ).then(() => {
+            btnMessage.textContent = message.success
+            form.reset()
+        }).catch(()=>{
+            btnMessage.textContent = message.fialer
+        }).finally(()=> {
+            btnMessage.textContent = "Send Message"
+        })
+    })
 })
